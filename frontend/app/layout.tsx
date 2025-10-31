ec827941e8ec661de475b2c83373c301a65be8cc
Saish Mungase <saishmungase@gmail.com>
2025-10-31 23:12:32 +0530

    init : basic frontend build

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/next'
+import './globals.css'
+
+
+export const metadata: Metadata = {
+  title: 'HomeROI',
+  description: 'The GOAT',
+  generator: 'Saish_AI',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`font-sans antialiased`}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  )
+}
